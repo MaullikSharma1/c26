2b08236012ce52602599adcd8318ab03dc6496b4
Maullik <parag11.libra@.com>
2020-11-19 19:01:14 +0530

    crates platform

--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var platform;
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,6 +19,9 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+
+    platform = new Ground(20,285,500,210)
+
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -59,4 +64,6 @@ function draw(){
     log5.display();
 
     bird.display();
+
+    platform.display();
 }
